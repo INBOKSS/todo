3b3cd0e64c1086f8c2929dac8021fa69ca5f4a62
Nikolajs Krasnovs <nikolay@co.inbox.lv>
2023-01-06 10:05:01 +0200

    Add initial set of files

---
+++ src/Entity/Todo.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TodoRepository::class)]
+class Todo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $note = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $done = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function isDone(): ?bool
+    {
+        return $this->done;
+    }
+
+    public function setDone(?bool $done): self
+    {
+        $this->done = $done;
+
+        return $this;
+    }
+}
